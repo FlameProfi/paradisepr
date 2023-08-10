857b22ca5b4e8b0f9499f4b86e6fab7e8c87c85b
beadidd <57589034+FlameProfi@users.noreply.github.com>
2023-08-11 01:29:13 +0300

    fixed recovery

--- dotnet/resources/NeptuneEvo/Accounts/Recovery/Repository.cs
+++ dotnet/resources/NeptuneEvo/Accounts/Recovery/Repository.cs
@@ -1,11 +1,15 @@
-﻿using Database;
+using System;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using Database;
 using GTANetworkAPI;
 using NeptuneEvo.Handles;
 using LinqToDB;
 using NeptuneEvo.Character;
 using NeptuneEvo.Chars;
 using NeptuneEvo.Players;
-using Redage.SDK;
+using Izumrud.SDK;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,11 +22,11 @@ namespace NeptuneEvo.Accounts.Recovery
     {
         private static readonly nLog Log = new nLog("Accounts.Repository.Events");
 
-        public static void SendEmail(ExtPlayer player, string login)
+        public static async Task SendEmailAsync(ExtPlayer player, string login)
         {
             try
             {
-	            var sessionData = player.GetSessionData();
+                var sessionData = player.GetSessionData();
                 if (sessionData == null) return;
                 var auntificationData = sessionData.AuntificationData;
 
@@ -46,14 +50,40 @@ namespace NeptuneEvo.Accounts.Recovery
                     Trigger.ClientEvent(player, "restorepassstep", 2);
                     return;
                 }
-                sessionData.RecoveryCode = Generate.RandomOneTimePassword();
-                if (sessionData.RecoveryCode == null)
+
+                string recoveryCode = Generate.RandomOneTimePassword();
+                if (recoveryCode == null)
                 {
                     Notify.Send(player, NotifyType.Error, NotifyPosition.BottomCenter, LangFunc.GetText(LangType.Ru, DataName.RecoveryError), 5000);
                     Trigger.ClientEvent(player, "restorepassstep", 2);
                     return;
                 }
-                Utils.Analytics.HelperThread.AddUrl($"recovery?email={auntificationData.Email}&name={login}&code={sessionData.RecoveryCode}&ip={sessionData.Address}");
+
+                string email = auntificationData.Email;
+                string subject = "Izumrud RP - восстановление пароля";
+                string body = $"Ваш код для восстановления: {recoveryCode}";
+
+                using (var smtpClient = new SmtpClient("Твой почтовый хост"))
+                {
+                    smtpClient.Port = 587;
+                    smtpClient.Credentials = new NetworkCredential("Твоя полная почта , пример - IzumrudRP@gmail.com", "IzumrudRP");
+                    smtpClient.EnableSsl = true;
+
+                    var mailMessage = new MailMessage
+                    {
+                        From = new MailAddress("Твоя полная почта , пример - IzumrudRP@gmail.com"),
+                        Subject = subject,
+                        Body = body,
+                        IsBodyHtml = false
+                    };
+
+                    mailMessage.To.Add(email);
+
+                    await smtpClient.SendMailAsync(mailMessage);
+                }
+
+                sessionData.RecoveryCode = recoveryCode;
+                Utils.Analytics.HelperThread.AddUrl($"recovery?email={auntificationData.Email}&name={login}&code={recoveryCode}&ip={sessionData.Address}");
                 Notify.Send(player, NotifyType.Success, NotifyPosition.BottomCenter, $"Сообщение с кодом для восстановления пароля успешно выслано на {Generate.ObfuscateEmail(auntificationData.Email)}.", 5000);
                 Trigger.ClientEvent(player, "restorepassstep", 1);
             }
@@ -62,11 +92,12 @@ namespace NeptuneEvo.Accounts.Recovery
                 Debugs.Repository.Exception(e);
             }
         }
+
         public static async void RecoveryPassword(ExtPlayer player, string code)
         {
             try
             {
-	            var sessionData = player.GetSessionData();
+                var sessionData = player.GetSessionData();
                 if (sessionData == null) return;
                 var auntificationData = sessionData.AuntificationData;
 
@@ -87,12 +118,12 @@ namespace NeptuneEvo.Accounts.Recovery
                         Trigger.ClientEvent(player, "restorepassstep", 2);
                         return;
                     }
-                    
+
                     Utils.Analytics.HelperThread.AddUrl($"newpassword?email={auntificationData.Email}&name={auntificationData.Login}&pass={newPassword}&ip={sessionData.Address}");
-                    
+
                     auntificationData.Password = Accounts.Repository.GetSha256(newPassword.ToString());
 
-                    await using var db = new ServerBD("MainDB");//В отдельном потоке
+                    await using var db = new ServerBD("MainDB"); // В отдельном потоке
 
                     await db.Accounts
                         .Where(v => v.Login == auntificationData.Login)
@@ -100,7 +131,7 @@ namespace NeptuneEvo.Accounts.Recovery
                         .UpdateAsync();
 
                     Autorization.Repository.AutorizationAccount(player, auntificationData.Login, auntificationData.Password).Wait();
-                    
+
                     Notify.Send(player, NotifyType.Success, NotifyPosition.BottomCenter, "Вы успешно восстановили доступ к аккаунту. Новый пароль выслан на емайл!", 5000);
                     Notify.Send(player, NotifyType.Success, NotifyPosition.BottomCenter, "Помимо этого Вы можете изменить его прямо сейчас из игры введя /password и новый желаемый пароль через пробел. Пример [/password 123] без скобок.", 10000);
                 }
